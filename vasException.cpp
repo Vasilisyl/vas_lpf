a19a015b3b63a42a7425d324184f3372f63921b8
vasilisyl <wang.maobo@outlook.com>
2020-11-26 00:32:28 +0800

    完善异常类vasException的实现

--- vasException.cpp
+++ vasException.cpp
@@ -10,28 +10,40 @@ extern "C" {
 }
 #endif
 
-VAS::vasException::vasException(const char *pFileName, int lineNum, const char *pFuncName, const std::string &description) throw()
-    : m_pFuncName(pFuncName), m_pFileName(pFileName), m_lineNum(lineNum), m_description(description)
+bool          VAS::g_isTraceEnabled = false;
+
+VAS::vasException::vasException(const char *pFileName, int lineNum, const char *pClassName, const char *pFuncName, const std::string &description) noexcept
+    : m_pClassName(pClassName),
+      m_pFuncName(pFuncName),
+      m_pFileName(pFileName),
+      m_lineNum(lineNum),
+      m_description(description)
 {
     m_description.append("\n");
     m_description.append(std::string("[FILE] ") + m_pFileName + " [LINE] " + std::to_string(m_lineNum) + "\n");
+    m_description.append(std::string("[CLASS] ") + m_pClassName + "\n");
     m_description.append(std::string("[FUNCTION] ") + m_pFuncName + "\n");
-    ::backtrace(m_traceArray, VAS_TRACE_MAX_SIZE);
+
+    if (g_isTraceEnabled) {
+        ::backtrace(m_traceArray, VAS_TRACE_MAX_SIZE);
+    }
 }
 
-VAS::vasException::~vasException() throw()
+VAS::vasException::~vasException() noexcept
 {
-
+    
 }
 
-const char *VAS::vasException::what() const throw()
+const char *VAS::vasException::what() const noexcept
 {
-    char **pStrList = ::backtrace_symbols(m_traceArray, VAS_TRACE_MAX_SIZE);
-    for (int i = 0; i < VAS_TRACE_MAX_SIZE; ++i) {
-        std::string tempStr(pStrList[i]);
-        if (!tempStr.empty()) {
-            m_description.append(tempStr);
-            m_description.append("\n");
+    if (g_isTraceEnabled && m_traceArray) {
+        char **pStrList = ::backtrace_symbols(m_traceArray, VAS_TRACE_MAX_SIZE);
+        for (int i = 0; i < VAS_TRACE_MAX_SIZE; ++i) {
+            std::string tempStr(pStrList[i]);
+            if (!tempStr.empty()) {
+                m_description.append(tempStr);
+                m_description.append("\n");
+            }
         }
     }
     return m_description.c_str();
--- vasException.h
+++ vasException.h
@@ -2,14 +2,15 @@
 
 #include <exception>
 #include <vector>
+#include <cstdlib>
 #include "vasDefined.h"
 
-#ifndef VAS_EXCEPTION
-    #define VAS_EXCEPTION(str) \
+#ifndef VAS_EXCEPTION_THROW
+    #define VAS_EXCEPTION_THROW(className, str) \
         { \
-            throw VAS::vasException(__FUNCTION__, __FILE__, __LINE__, str); \
+            throw VAS::vasException(__FILE__, __LINE__, #className, __FUNCTION__, str); \
         }
-#endif /*VAS_EXCEPTION*/
+#endif /*VAS_EXCEPTION_THROW*/
 
 #ifndef VAS_TRACE_MAX_SIZE
     #define VAS_TRACE_MAX_SIZE 50
@@ -17,24 +18,36 @@
 
 namespace VAS {
 
+extern bool         g_isTraceEnabled;
+
 /**
  * @brief vasException 异常类
  */
 VAS_USER_CLASS vasException : public std::exception
 {
 public:
-    vasException(const char *pFileName, int lineNum, const char *pFuncName, const std::string &description) throw();
-    virtual ~vasException() throw();
+    vasException(const char *pFileName, int lineNum, const char *pClassName, \
+        const char *pFuncName, const std::string &description) noexcept;
+    virtual ~vasException() noexcept;
 
-    virtual const char *what() const throw() override;
+    virtual const char *what() const noexcept override;
+
+    inline std::string getExceptionClassName() const noexcept { return m_pClassName; }
+
+    inline std::string getExceptionFuncName() const noexcept { return m_pFuncName; }
+
+    inline std::string getExceptionFileName() const noexcept { return m_pFileName; }
+
+    inline int getExceptionFileLineNumber() const noexcept { return m_lineNum; };
 
 protected:
+    const char *const   m_pClassName;
     const char *const   m_pFuncName;
     const char *const   m_pFileName;
     const int           m_lineNum;
     mutable std::string m_description;
 
-    void *              m_traceArray[VAS_TRACE_MAX_SIZE];
+    void               *m_traceArray[VAS_TRACE_MAX_SIZE];
 };
 
 } /*namespace VAS*/

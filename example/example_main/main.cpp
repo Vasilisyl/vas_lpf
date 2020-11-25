cd422398b8dfc6972444293cc81a55559610e32c
vasilisyl <wang.maobo@outlook.com>
2020-11-26 00:35:15 +0800

    example中新增异常的处理

--- example/example_main/main.cpp
+++ example/example_main/main.cpp
@@ -22,7 +22,7 @@ void stopPlugins(const std::vector<std::string> &pluginIdVec);
 
 /*批量卸载插件*/
 void uninstallPlugins(const std::vector<std::string> &pluginIdVec);
-
+#include <thread>
 int main(int argc, char *argv[])
 {
     std::cout << "install ......" << std::endl;
@@ -37,21 +37,28 @@ int main(int argc, char *argv[])
     pluginPathVec.push_back(std::string(pWorkPath) + "/../plugins/libplugin_test.so"); /*测试插件plugin.test*/
 #endif
 
-    /*安装插件*/
-    std::vector<std::string> pluginIdVec = ::installPlugins(pluginPathVec);
-    if (pluginIdVec.empty()) {
+    try {
+
+        /*安装插件*/
+        std::vector<std::string> pluginIdVec = ::installPlugins(pluginPathVec);
+        if (pluginIdVec.empty()) {
+            return -1;
+        }
+
+        /*启动插件*/
+        /*plugin.test插件将在启动时通过触发事件的方式，完成与plugin.demo插件之间的数据交互*/
+        ::startPlugins(pluginIdVec);
+
+        /*停止插件*/
+        ::stopPlugins(pluginIdVec);
+
+        /*卸载插件*/
+        ::uninstallPlugins(pluginIdVec);
+
+    } catch (const VAS::vasException &e) {
+        std::cout << e.what() << std::endl;
         return -1;
     }
-
-    /*启动插件*/
-    /*plugin.test插件将在启动时通过触发事件的方式，完成与plugin.demo插件之间的数据交互*/
-    ::startPlugins(pluginIdVec);
-
-    /*停止插件*/
-    ::stopPlugins(pluginIdVec);
-
-    /*卸载插件*/
-    ::uninstallPlugins(pluginIdVec);
     
     return 0;
 }
@@ -62,14 +69,8 @@ std::vector<std::string> installPlugins(const std::vector<std::string> &pluginPa
     std::vector<std::string> pluginIdVec;
     std::string pluginId;
     for (const std::string &pluginPath : pluginPathVec) {
-        pluginId = VAS_PLUGIN_CONTEXT->install(pluginPath, &err);
-        if (pluginId.empty()) {
-            std::cout << "install plugin ( path = " << pluginPath << " ) failed ! reason: " << err << std::endl;
-        }
-        else {
-            std::cout << "install plugin ( path = " << pluginPath << " id =  " << pluginId << " ) successfully !" << std::endl;
-            pluginIdVec.push_back(pluginId);
-        }
+        pluginId = VAS_PLUGIN_CONTEXT->install(pluginPath);
+        pluginIdVec.push_back(pluginId);
     }
     return pluginIdVec;
 }
@@ -78,12 +79,7 @@ void startPlugins(const std::vector<std::string> &pluginIdVec)
 {
     std::string err;
     for (const std::string &pluginId : pluginIdVec) {
-        if (VAS_PLUGIN_CONTEXT->startPlugin(pluginId, &err)) {
-            std::cout << "start plugin ( id = " << pluginId << " ) successfully !" << std::endl;
-        }
-        else {
-            std::cout << "start plugin ( id = " << pluginId << " ) failed ! reason: " << err << std::endl;
-        }
+        VAS_PLUGIN_CONTEXT->startPlugin(pluginId);
     }
 }
 
@@ -91,12 +87,7 @@ void stopPlugins(const std::vector<std::string> &pluginIdVec)
 {
     std::string err;
     for (const std::string &pluginId : pluginIdVec) {
-        if (VAS_PLUGIN_CONTEXT->stopPlugin(pluginId, &err)) {
-            std::cout << "stop plugin ( id = " << pluginId << " ) successfully !" << std::endl;
-        }
-        else {
-            std::cout << "stop plugin ( id = " << pluginId << " ) failed ! reason: " << err << std::endl;
-        }
+        VAS_PLUGIN_CONTEXT->stopPlugin(pluginId);
     }
 }
 
@@ -104,11 +95,6 @@ void uninstallPlugins(const std::vector<std::string> &pluginIdVec)
 {
     std::string err;
     for (const std::string &pluginId : pluginIdVec) {
-        if (VAS_PLUGIN_CONTEXT->uninstall(pluginId, &err)) {
-            std::cout << "uninstall plugin ( id = " << pluginId << " ) successfully !" << std::endl;
-        }
-        else {
-            std::cout << "uninstall plugin ( id = " << pluginId << " ) failed ! reason: " << err << std::endl;
-        }
+        VAS_PLUGIN_CONTEXT->uninstall(pluginId);
     }
 }

a1cf7643175d5eb2c6d5f75d617acbc50f37eb08
vasilisyl <wang.maobo@outlook.com>
2020-11-25 15:53:10 +0800

    添加异常类vasException的实现

--- vasException.cpp
+++ vasException.cpp
@@ -10,3 +10,29 @@ extern "C" {
 }
 #endif
 
+VAS::vasException::vasException(const char *pFileName, int lineNum, const char *pFuncName, const std::string &description) throw()
+    : m_pFuncName(pFuncName), m_pFileName(pFileName), m_lineNum(lineNum), m_description(description)
+{
+    m_description.append("\n");
+    m_description.append(std::string("[FILE] ") + m_pFileName + " [LINE] " + std::to_string(m_lineNum) + "\n");
+    m_description.append(std::string("[FUNCTION] ") + m_pFuncName + "\n");
+    ::backtrace(m_traceArray, VAS_TRACE_MAX_SIZE);
+}
+
+VAS::vasException::~vasException() throw()
+{
+
+}
+
+const char *VAS::vasException::what() const throw()
+{
+    char **pStrList = ::backtrace_symbols(m_traceArray, VAS_TRACE_MAX_SIZE);
+    for (int i = 0; i < VAS_TRACE_MAX_SIZE; ++i) {
+        std::string tempStr(pStrList[i]);
+        if (!tempStr.empty()) {
+            m_description.append(tempStr);
+            m_description.append("\n");
+        }
+    }
+    return m_description.c_str();
+}
--- vasException.h
+++ vasException.h
@@ -1,7 +1,19 @@
 #pragma once
 
 #include <exception>
+#include <vector>
 #include "vasDefined.h"
+
+#ifndef VAS_EXCEPTION
+    #define VAS_EXCEPTION(str) \
+        { \
+            throw VAS::vasException(__FUNCTION__, __FILE__, __LINE__, str); \
+        }
+#endif /*VAS_EXCEPTION*/
+
+#ifndef VAS_TRACE_MAX_SIZE
+    #define VAS_TRACE_MAX_SIZE 50
+#endif /*VAS_TRACE_MAX_SIZE*/
 
 namespace VAS {
 
@@ -11,7 +23,7 @@ namespace VAS {
 class vasException : public std::exception
 {
 public:
-    vasException(const char *pFuncName, const char *pFileName, int lineNum, const std::string &description) throw();
+    vasException(const char *pFileName, int lineNum, const char *pFuncName, const std::string &description) throw();
     virtual ~vasException() throw();
 
     virtual const char *what() const throw() override;
@@ -21,6 +33,8 @@ protected:
     const char *const   m_pFileName;
     const int           m_lineNum;
     mutable std::string m_description;
+
+    void *              m_traceArray[VAS_TRACE_MAX_SIZE];
 };
 
 } /*namespace VAS*/
